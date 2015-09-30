0e9058c29fe0623e4985dc52a4c982750d7035d7
xxnatc <cychownat@gmail.com>
2015-09-30 13:46:53 -0700

    Fixed typos

--- game.js
+++ game.js
@@ -6,14 +6,14 @@ if (question1.toLowerCase() === 'no' || question1.toLowerCase() === 'n') {
 }
 
 var question2 = prompt('Have I ever lived in New York City?');
-if (question2.toLowerCase() === 'no' || question1.toLowerCase() === 'n') {
+if (question2.toLowerCase() === 'no' || question2.toLowerCase() === 'n') {
   alert('You are correct! I have lived in Hong Kong, London, Seattle, but not New York City.');
 } else {
   alert('Looks like I tricked you! Truth is, I have never set foot in New York City in my life.');
 }
 
 var question3 = prompt('Do I prefer coffee over tea?');
-if (question3.toLowerCase() === 'yes' || question1.toLowerCase() === 'y') {
+if (question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y') {
   alert('That\'s right! Coffee flows in my blood.');
 } else {
   alert('You got it wrong! COFFEE FOR LIFE!!!!');
