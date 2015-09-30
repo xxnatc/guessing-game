208b3cc128bc52a127338eba076416e7e08d2f8e
Natalie Chow <cychownat@gmail.com>
2015-09-30 13:34:29 -0700

    Added error correction

--- game.js
+++ game.js
@@ -1,27 +1,20 @@
 var question1 = prompt('Am I a dog person?');
-if (question1.toLowerCase() === 'no') {
+if (question1.toLowerCase() === 'no' || question1.toLowerCase() === 'n') {
   alert('You\'re off to a great start! I am very much a cat person.');
 } else {
   alert('Sorry! I am actually a cat person.');
 }
 
 var question2 = prompt('Have I ever lived in New York City?');
-if (question2.toLowerCase() === 'no') {
+if (question2.toLowerCase() === 'no' || question1.toLowerCase() === 'n') {
   alert('You are correct! I have lived in Hong Kong, London, Seattle, but not New York City.');
 } else {
-  alert('Looks like I tricked you! Truth is, I have never set foot in New York City in my foot.');
+  alert('Looks like I tricked you! Truth is, I have never set foot in New York City in my life.');
 }
 
 var question3 = prompt('Do I prefer coffee over tea?');
-if (question3.toLowerCase() === 'yes') {
+if (question3.toLowerCase() === 'yes' || question1.toLowerCase() === 'y') {
   alert('That\'s right! Coffee flows in my blood.');
 } else {
   alert('You got it wrong! COFFEE FOR LIFE!!!!');
 }
-// console.log('Question 1 response: ' + question1);
-// console.log('Question 2 response: ' + question2);
-// console.log('Question 3 response: ' + question3);
-
-// alert('You answered question 1 with: "' + question1 + '".');
-// alert('You answered question 2 with: "' + question2 + '".');
-// alert('You answered question 3 with: "' + question3 + '".');
