3813056f41fbb146405de06abed8de0d9ec3138e
Natalie Chow <cychownat@gmail.com>
2015-09-30 13:51:30 -0700

    Added score and name tracking, summary message

--- game.js
+++ game.js
@@ -1,20 +1,28 @@
+var name = prompt('Welcome! What\'s your name?');
+var score = 0;
+
 var question1 = prompt('Am I a dog person?');
 if (question1.toLowerCase() === 'no' || question1.toLowerCase() === 'n') {
-  alert('You\'re off to a great start! I am very much a cat person.');
+  alert('You\'re off to a great start, ' + name + '! I am very much a cat person.');
+  score++;
 } else {
-  alert('Sorry! I am actually a cat person.');
+  alert('Sorry, ' + name + '! I am actually a cat person.');
 }
 
 var question2 = prompt('Have I ever lived in New York City?');
-if (question2.toLowerCase() === 'no' || question1.toLowerCase() === 'n') {
+if (question2.toLowerCase() === 'no' || question2.toLowerCase() === 'n') {
   alert('You are correct! I have lived in Hong Kong, London, Seattle, but not New York City.');
+  score++;
 } else {
-  alert('Looks like I tricked you! Truth is, I have never set foot in New York City in my life.');
+  alert('Looks like I tricked you! The truth is, ' + name + ', I have never set foot in New York City in my life.');
 }
 
 var question3 = prompt('Do I prefer coffee over tea?');
-if (question3.toLowerCase() === 'yes' || question1.toLowerCase() === 'y') {
-  alert('That\'s right! Coffee flows in my blood.');
+if (question3.toLowerCase() === 'yes' || question3.toLowerCase() === 'y') {
+  alert('Well done, ' + name + '! Coffee flows in my blood.');
+  score++;
 } else {
-  alert('You got it wrong! COFFEE FOR LIFE!!!!');
+  alert('You got it wrong, ' + name + '! COFFEE FOR LIFE!!!!');
 }
+
+alert('You got ' + score + ' out of 3 correct, ' + name + '. Thanks for playing!');
