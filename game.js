8b805f7be6f45098167f99c300a5eb1c0c5fe7ab
Natalie Chow <cychownat@gmail.com>
2015-09-30 12:04:29 -0700

    This is the initial commit

---
+++ game.js
@@ -0,0 +1,11 @@
+var question1 = prompt('Am I a dog person?');
+var question2 = prompt('Have I ever lived in London?');
+var question3 = prompt('Do I prefer coffee over tea?');
+
+console.log('Question 1 response: ' + question1);
+console.log('Question 2 response: ' + question2);
+console.log('Question 3 response: ' + question3);
+
+alert('You answered question 1 with: "' + question1 + '".');
+alert('You answered question 2 with: "' + question2 + '".');
+alert('You answered question 3 with: "' + question3 + '".');
