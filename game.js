9b79181d376b97c02a572425da866723315c5817
David Swaintek <dswaintek@gmail.com>
2015-10-02 12:45:54 -0700

    cleaned up code style

--- game.js
+++ game.js
@@ -11,11 +11,11 @@ var answers = ['yes', 'y', 'no', 'n'];
 function ques1() {
   var question1 = prompt(questions[0]);
   if (question1.toLowerCase() === answers[2] || question1.toLowerCase() === answers[3]) {
-    one.className='correct';
+    one.className = 'correct';
     one.innerHTML = 'You\'re off to a great start, ' + name + '! I am very much a cat person.' + '<br /><img src="images/cat.jpg" height="200" width="200" />';
     score++;
   } else {
-    one.className='incorrect';
+    one.className = 'incorrect';
     one.innerHTML = 'Sorry, ' + name + '! I am actually a cat person.';
   }
 }
@@ -23,11 +23,11 @@ function ques1() {
 function ques2() {
   var question2 = prompt(questions[1]);
   if (question2.toLowerCase() === answers[2] || question2.toLowerCase() === answers[3]) {
-    two.className='correct';
+    two.className = 'correct';
     two.innerHTML = 'You are correct! I have lived in Hong Kong, London, Seattle, but not New York City.' + '<br /><img src="images/seattle.jpg" height="200" width="200" />';
     score++;
   } else {
-    two.className='incorrect';
+    two.className = 'incorrect';
     two.innerHTML = 'Looks like I tricked you! The truth is, ' + name + ', I have never set foot in New York City in my life.';
   }
 }
@@ -35,11 +35,11 @@ function ques2() {
 function ques3() {
   var question3 = prompt(questions[2]);
   if (question3.toLowerCase() === answers[0] || question3.toLowerCase() === answers[1]) {
-    three.className='correct';
+    three.className = 'correct';
     three.innerHTML = 'Well done, ' + name + '! Coffee flows in my blood.' + '<br /><img src="images/coffee.jpg" height="200" width="200" />';
     score++;
   } else {
-    three.className='incorrect';
+    three.className = 'incorrect';
     three.innerHTML = 'You got it wrong, ' + name + '! COFFEE FOR LIFE!!!!';
   }
 }
